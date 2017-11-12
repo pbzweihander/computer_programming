7b2afe7e5f049de05177d8c755589a13302d6117
pbzweihander <sd852456@naver.com>
2017-11-12 20:45:13 +0900

    RETAB

--- assignment03/cube/Assignment3_Cube.java
+++ assignment03/cube/Assignment3_Cube.java
@@ -1,24 +1,25 @@
 import java.io.*;
+
 public class Assignment3_Cube {
-	private Assignment3_Side[] side;
-	private int size;
+    private Assignment3_Side[] side;
+    private int size;
 
-	public Assignment3_Cube(String filename) {
-		side = new Assignment3_Side[6];
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(filename));
+    public Assignment3_Cube(String filename) {
+        side = new Assignment3_Side[6];
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
 
-			size = Integer.parseInt(br.readLine());
-			for(int i = 0; i < 6; i++)
-				side[i] = new Assignment3_Side(size, i+1);
-			
-			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            size = Integer.parseInt(br.readLine());
+            for (int i = 0; i < 6; i++)
+                side[i] = new Assignment3_Side(size, i + 1);
 
-	public int[][] get(int s) {
-		return side[s-1].get();
-	}
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int[][] get(int s) {
+        return side[s - 1].get();
+    }
 }
--- assignment03/cube/Assignment3_Side.java
+++ assignment03/cube/Assignment3_Side.java
@@ -1,13 +1,14 @@
 public class Assignment3_Side {
-	private int[][] side;
-	public Assignment3_Side(int size, int value) {
-		side = new int[size][size];
-		for(int i = 0; i < size; i++)
-			for(int j = 0; j < size; j++)
-				side[i][j] = value;
-	}
+    private int[][] side;
 
-	public int[][] get() {
-		return side;
-	}
+    public Assignment3_Side(int size, int value) {
+        side = new int[size][size];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                side[i][j] = value;
+    }
+
+    public int[][] get() {
+        return side;
+    }
 }
