87e18f36fbf7c6523c26de6347fa8b53ddd02109
pbzweihander <sd852456@naver.com>
2017-11-21 22:33:02 +0900

    seems done

--- assignment04/tree/src/Assignment4_1.java
+++ assignment04/tree/src/Assignment4_1.java
@@ -32,26 +32,44 @@ public class Assignment4_1 {
         }
 
         public T pop() {
+            if (root == null)
+                return null;
             final T t = root.value;
             root = root.next;
             return t;
         }
 
-        public int getSize() {
-            if (root == null)
-                return 0;
-            int n = 1;
-            Node cursor = root;
-            while (cursor.next != null) {
-                n++;
-                cursor = cursor.next;
-            }
-            return n;
+        public boolean isEmpty() {
+            return root == null;
         }
     }
 
     public Assignment4_1(String line1, String line2) {
+        String str_queue = "";
+        Queue<Node4_1> node_queue = new Queue<>();
+        Queue<Node4_1> child_needed_queue = new Queue<>();
+        int i = 0;
+        int j = 0;
+        for (char c : line1.toCharArray()) {
+            Node4_1 node = new Node4_1();
+            boolean is_leaf = c == '1';
+            node.label = c;
+            node.character = is_leaf ? '1' : line2.charAt(i++);
+            node_queue.push(node);
+        }
 
+        root = node_queue.pop();
+        child_needed_queue.push(root);
+
+        while (!child_needed_queue.isEmpty()) {
+            Node4_1 n = child_needed_queue.pop();
+            n.left = node_queue.pop();
+            n.right = node_queue.pop();
+            if (n.left != null && n.left.label == '0')
+                child_needed_queue.push(n.left);
+            if (n.right != null && n.right.label == '0')
+                child_needed_queue.push(n.right);
+        }
     }
 
     public Node4_1 getRoot() {
