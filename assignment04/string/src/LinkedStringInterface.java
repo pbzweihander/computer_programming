bc9cde285052718ae2820294850eb75fc9a1546c
pbzweihander <sd852456@naver.com>
2017-11-21 11:05:29 +0900

    format

--- assignment04/string/src/LinkedStringInterface.java
+++ assignment04/string/src/LinkedStringInterface.java
@@ -1,14 +1,29 @@
 public interface LinkedStringInterface {
     void remove(String substr);
+
     void remove(LinkedStringInterface substr);
+
     String toString();
+
     char charAt(int index);
+
     int length();
+
     LinkedStringInterface substring(int startIndex, int endIndex);
+
     boolean contains(LinkedStringInterface substr);
+
     boolean contains(String substr);
-    int compareTo(LinkedStringInterface str);  int compareTo(String str);
-    int compareToIgnoreCase(LinkedStringInterface str);  int compareToIgnoreCase(String str);
+
+    int compareTo(LinkedStringInterface str);
+
+    int compareTo(String str);
+
+    int compareToIgnoreCase(LinkedStringInterface str);
+
+    int compareToIgnoreCase(String str);
+
     LinkedStringInterface concat(LinkedStringInterface str);
+
     LinkedStringInterface concat(String str);
 }
