1e3142fbb4f5f8ad2606680343accc67bde68958
pbzweihander <sd852456@naver.com>
2017-11-21 10:56:24 +0900

    fixed typo

--- assignment04/tree/src/Tester4_1.java
+++ assignment04/tree/src/Tester4_1.java
@@ -2,9 +2,11 @@ public class Tester4_1 {
     public static void main(String[] args) {
         Assignment4_1 a = new Assignment4_1("00001101111", "ABCDE");
         Node4_1 r = a.getRoot();
+        //reports in bits
         System.out.println(preorder(r));
         System.out.println(inorder(r));
         System.out.println(postorder(r));
+        //reports in character
         System.out.println(preorderLetter(r));
         System.out.println(inorderLetter(r));
         System.out.println(postorderLetter(r));
@@ -43,7 +45,7 @@ public class Tester4_1 {
     public static String preorderLetter(Node4_1 n) {
         String s = "";
         if (n.label == '0')
-            s += n.c;
+            s += n.character;
         if (n.left != null)
             s += preorderLetter(n.left);
         if (n.right != null)
@@ -56,7 +58,7 @@ public class Tester4_1 {
         if (n.left != null)
             s += inorderLetter(n.left);
         if (n.label == '0')
-            s += n.c;
+            s += n.character;
         if (n.right != null)
             s += inorderLetter(n.right);
         return s;
@@ -69,7 +71,7 @@ public class Tester4_1 {
         if (n.right != null)
             s += postorderLetter(n.right);
         if (n.label == '0')
-            s += n.c;
+            s += n.character;
         return s;
     }
 
