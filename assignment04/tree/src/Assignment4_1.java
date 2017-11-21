7bd3222b8160992893a472bca7937cced6759e94
pbzweihander <sd852456@naver.com>
2017-11-21 11:07:21 +0900

    format

--- assignment04/tree/src/Assignment4_1.java
+++ assignment04/tree/src/Assignment4_1.java
@@ -1,7 +1,9 @@
 public class Assignment4_1 {
     public Node4_1 root;
+
     public Assignment4_1(String line1, String line2) {
     }
+
     public Node4_1 getRoot() {
         return root;
     }
