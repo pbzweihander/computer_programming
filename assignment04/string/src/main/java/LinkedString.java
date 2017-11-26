860c3dfcf58c70c157c68d1090c4078d71a71454
pbzweihander <sd852456@naver.com>
2017-11-27 04:29:10 +0900

    remove

--- assignment04/string/src/main/java/LinkedString.java
+++ assignment04/string/src/main/java/LinkedString.java
@@ -106,11 +106,69 @@ public class LinkedString implements LinkedStringInterface {
     }
 
     public void remove(String substr) {
-        // TODO: remove of String
+        char[] pattern = substr.toCharArray();
+        int pattern_len = pattern.length;
+        if (substr.isEmpty() || isEmpty() || pattern_len > length())
+            return;
+        CharacterNode str_cursor = new CharacterNode('\0');
+        str_cursor.next = root;
+        int pattern_cursor = 0;
+        while (str_cursor.next != null) {
+            if (str_cursor.next.value == pattern[pattern_cursor]) {
+                CharacterNode str_matching_node = str_cursor.next.next;
+                pattern_cursor++;
+                int count = 1;
+                while (str_matching_node != null) {
+                    if (str_matching_node.value == pattern[pattern_cursor]) {
+                        count++;
+                        if (count == pattern_len) {
+                            str_cursor.next = str_matching_node.next;
+                            remove(substr);
+                            return;
+                        }
+                        str_matching_node = str_matching_node.next;
+                        pattern_cursor++;
+                    } else
+                        break;
+                }
+                str_cursor = str_cursor.next;
+                pattern_cursor = 0;
+            } else
+                str_cursor = str_cursor.next;
+        }
     }
 
     public void remove(LinkedStringInterface substr) {
-        // TODO: remove of LinkedString
+        LinkedString pattern = (LinkedString) substr;
+        int pattern_len = pattern.length();
+        if (pattern.isEmpty() || isEmpty() || pattern_len > length())
+            return;
+        CharacterNode str_cursor = new CharacterNode('\0');
+        str_cursor.next = root;
+        CharacterNode pattern_cursor = pattern.root;
+        while (str_cursor.next != null) {
+            if (str_cursor.next.value == pattern_cursor.value) {
+                CharacterNode str_matching_node = str_cursor.next.next;
+                pattern_cursor = pattern_cursor.next;
+                int count = 1;
+                while (str_matching_node != null) {
+                    if (str_matching_node.value == pattern_cursor.value) {
+                        count++;
+                        if (count == pattern_len) {
+                            str_cursor.next = str_matching_node.next;
+                            remove(substr);
+                            return;
+                        }
+                        str_matching_node = str_matching_node.next;
+                        pattern_cursor = pattern_cursor.next;
+                    } else
+                        break;
+                }
+                str_cursor = str_cursor.next;
+                pattern_cursor = pattern.root;
+            } else
+                str_cursor = str_cursor.next;
+        }
     }
 
     public String toString() {
