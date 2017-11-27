604741b7cd92d89340cdeac1fafd4497385d90ba
pbzweihander <sd852456@naver.com>
2017-11-27 20:12:43 +0900

    more test

--- assignment04/string/src/test/java/LinkedStringTest.java
+++ assignment04/string/src/test/java/LinkedStringTest.java
@@ -5,15 +5,26 @@ import java.util.Random;
 public class LinkedStringTest {
     private final String lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
+    private String randomString(int len) {
+        byte[] bytes = new byte[len];
+        Random random = new Random();
+        random.nextBytes(bytes);
+        return new String(bytes);
+    }
+
+    private int sign(int n) {
+        return n > 0 ? 1 : (n == 0 ? 0 : -1);
+    }
+
     @Test
     public void testSubstring() {
         LinkedString str = new LinkedString(lorem_ipsum);
         Random random = new Random();
         int startIndex = random.nextInt(lorem_ipsum.length() / 2);
         int endIndex = lorem_ipsum.length() / 2 + random.nextInt(lorem_ipsum.length() / 2 - 1);
-        String norm_substring = lorem_ipsum.substring(startIndex, endIndex);
-        String linked_substring = str.substring(startIndex, endIndex).toString();
-        assertTrue(norm_substring.equals(linked_substring));
+        assertTrue(lorem_ipsum.substring(startIndex, endIndex).equals(str.substring(startIndex, endIndex).toString()));
+        assertTrue(
+                lorem_ipsum.substring(startIndex, startIndex).equals(str.substring(startIndex, startIndex).toString()));
     }
 
     @Test
@@ -54,12 +65,12 @@ public class LinkedStringTest {
         String str3 = randomString(lorem_ipsum.length() / 2 - random.nextInt(lorem_ipsum.length() / 2 - 1));
         String str4 = randomString(lorem_ipsum.length() / 2 + random.nextInt(lorem_ipsum.length() / 2 - 1));
         LinkedString str = new LinkedString(str1);
-        assertEquals(str1.compareTo(str2), str.compareTo(str2));
-        assertEquals(str1.compareTo(str2), str.compareTo(new LinkedString(str2)));
-        assertEquals(str1.compareTo(str3), str.compareTo(str3));
-        assertEquals(str1.compareTo(str3), str.compareTo(new LinkedString(str3)));
-        assertEquals(str1.compareTo(str4), str.compareTo(str4));
-        assertEquals(str1.compareTo(str4), str.compareTo(new LinkedString(str4)));
+        assertEquals(sign(str1.compareTo(str2)), sign(str.compareTo(str2)));
+        assertEquals(sign(str1.compareTo(str2)), sign(str.compareTo(new LinkedString(str2))));
+        assertEquals(sign(str1.compareTo(str3)), sign(str.compareTo(str3)));
+        assertEquals(sign(str1.compareTo(str3)), sign(str.compareTo(new LinkedString(str3))));
+        assertEquals(sign(str1.compareTo(str4)), sign(str.compareTo(str4)));
+        assertEquals(sign(str1.compareTo(str4)), sign(str.compareTo(new LinkedString(str4))));
     }
 
     @Test
@@ -70,19 +81,12 @@ public class LinkedStringTest {
         String str3 = randomString(lorem_ipsum.length() / 2 - random.nextInt(lorem_ipsum.length() / 2 - 1));
         String str4 = randomString(lorem_ipsum.length() / 2 + random.nextInt(lorem_ipsum.length() / 2 - 1));
         LinkedString str = new LinkedString(str1);
-        assertEquals(str1.compareToIgnoreCase(str2), str.compareToIgnoreCase(str2));
-        assertEquals(str1.compareToIgnoreCase(str2), str.compareToIgnoreCase(new LinkedString(str2)));
-        assertEquals(str1.compareToIgnoreCase(str3), str.compareToIgnoreCase(str3));
-        assertEquals(str1.compareToIgnoreCase(str3), str.compareToIgnoreCase(new LinkedString(str3)));
-        assertEquals(str1.compareToIgnoreCase(str4), str.compareToIgnoreCase(str4));
-        assertEquals(str1.compareToIgnoreCase(str4), str.compareToIgnoreCase(new LinkedString(str4)));
-    }
-
-    private String randomString(int len) {
-        byte[] bytes = new byte[len];
-        Random random = new Random();
-        random.nextBytes(bytes);
-        return new String(bytes);
+        assertEquals(sign(str1.compareToIgnoreCase(str2)), sign(str.compareToIgnoreCase(str2)));
+        assertEquals(sign(str1.compareToIgnoreCase(str2)), sign(str.compareToIgnoreCase(new LinkedString(str2))));
+        assertEquals(sign(str1.compareToIgnoreCase(str3)), sign(str.compareToIgnoreCase(str3)));
+        assertEquals(sign(str1.compareToIgnoreCase(str3)), sign(str.compareToIgnoreCase(new LinkedString(str3))));
+        assertEquals(sign(str1.compareToIgnoreCase(str4)), sign(str.compareToIgnoreCase(str4)));
+        assertEquals(sign(str1.compareToIgnoreCase(str4)), sign(str.compareToIgnoreCase(new LinkedString(str4))));
     }
 
     @Test
