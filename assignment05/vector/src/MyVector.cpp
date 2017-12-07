5080184df1c3c64bfccab3ff32291b1d393fa49b
pbzweihander <sd852456@naver.com>
2017-12-07 11:22:32 +0900

    seems done

--- assignment05/vector/src/MyVector.cpp
+++ assignment05/vector/src/MyVector.cpp
@@ -1,45 +1,136 @@
 #include "MyVector.h"
 
-MyVector::MyVector(double numList[], int size) {
+void arrcpy(double dest[], const double from[], int size) {
+    for(int i = 0; i < size; i++)
+        dest[i] = from[i];
+}
 
+MyVector::MyVector(double numList[], int dimension) {
+    this->dimension = dimension;
+    numbers = new double[dimension];
+    arrcpy(this->numbers, numList, dimension);
 }
 
 MyVector::MyVector() {
-
+    dimension = 0;
+    numbers = new double[0];
 }
 
-void print() {
-
+string MyVector::to_string() const {
+    if (dimension == 0)
+        return "()";
+    stringstream s;
+    s << "(";
+    for (int i = 0; i < dimension - 1; i++)
+        s << numbers[i] << ", ";
+    s << numbers[dimension - 1] << ")";
+    return s.str();
 }
 
-MyVector operator+(MyVector vec) {
-   return MyVector();
+void MyVector::print() const {
+    cout << to_string() << endl;
 }
 
-double operator*(MyVector vec) {
-   return 0.0;
+int MyVector::get_dimension() const {
+    return dimension;
 }
 
-MyVector operator/(MyVector a, MyVector b) {
-   return MyVector();
+double MyVector::get(int index) const {
+    return numbers[index];
 }
 
-MyVector operator-(MyVector vec) {
-   return MyVector();
+MyVector MyVector::operator+(const MyVector& vec) const {
+    if (dimension != vec.get_dimension()) {
+        print_error('+', *this, vec);
+        return MyVector();
+    }
+    double* n = new double[dimension];
+    for (int i = 0; i < dimension; i++)
+        n[i] = numbers[i] + vec.get(i);
+    return MyVector(n, dimension);
 }
 
-MyVector operator+(double num, MyVector vec) {
+MyVector MyVector::operator-(const MyVector& vec) const {
+    if (dimension != vec.get_dimension()) {
+        print_error('-', *this, vec);
+        return MyVector();
+    }
+    double* n = new double[dimension];
+    for (int i = 0; i < dimension; i++)
+        n[i] = numbers[i] - vec.get(i);
+    return MyVector(n, dimension);
+}
+
+double MyVector::operator*(const MyVector& vec) const {
+    if (dimension != vec.get_dimension()) {
+        print_error('*', *this, vec);
+        return 0.0;
+    }
+    double d = 0.0;
+    for (int i = 0; i < dimension; i++)
+        d += numbers[i] * vec.get(i);
+    return d;
+}
+
+MyVector MyVector::operator/(const MyVector& vec) const {
+    print_error('/', *this, vec);
     return MyVector();
 }
 
-MyVector operator-(double num, MyVector vec) {
+MyVector operator+(double num, const MyVector& vec) {
+    double* d = new double[vec.get_dimension()];
+    for (int i = 0; i < vec.get_dimension(); i++)
+        d[i] = vec.get(i) + num;
+    return MyVector(d, vec.get_dimension());
+}
+
+MyVector operator+(const MyVector& vec, double num) {
+    return num + vec;
+}
+
+MyVector operator-(double num, const MyVector& vec) {
+    print_error('-', num, vec);
     return MyVector();
 }
 
-MyVector operator/(double num, MyVector vec) {
+MyVector operator-(const MyVector& vec, double num) {
+    double* d = new double[vec.get_dimension()];
+    for (int i = 0; i < vec.get_dimension(); i++)
+        d[i] = vec.get(i) - num;
+    return MyVector(d, vec.get_dimension());
+}
+
+MyVector operator*(double num, const MyVector& vec) {
+    double* d = new double[vec.get_dimension()];
+    for (int i = 0; i < vec.get_dimension(); i++)
+        d[i] = vec.get(i) * num;
+    return MyVector(d, vec.get_dimension());
+}
+
+MyVector operator*(const MyVector& vec, double num) {
+    return num * vec;
+}
+
+MyVector operator/(double num, const MyVector& vec) {
+    print_error('/', num, vec);
     return MyVector();
 }
 
-MyVector operator*(double num, MyVector vec) {
-    return MyVector();
+MyVector operator/(const MyVector& vec, double num) {
+    double* d = new double[vec.get_dimension()];
+    for (int i = 0; i < vec.get_dimension(); i++)
+        d[i] = vec.get(i) / num;
+    return MyVector(d, vec.get_dimension());
+}
+
+void print_error(char op, const MyVector& a, const MyVector& b) {
+    stringstream s;
+    s << op << " cannot applied to " << a.to_string() << " and " << b.to_string();
+    cout << s.str() << endl;
+}
+
+void print_error(char op, double a, const MyVector& b) {
+    stringstream s;
+    s << op << " cannot applied to " << a << " and " << b.to_string();
+    cout << s.str() << endl;
 }
--- assignment05/vector/src/MyVector.h
+++ assignment05/vector/src/MyVector.h
@@ -1,18 +1,31 @@
-#include <assert.h>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
 
 #ifndef MYVECTOR_H_INCLUDED
 #define MYVECTOR_H_INCLUDED
 
-class MyVector{
-   private:
-   public:
-    MyVector(double numList[], int size);
+class MyVector {
+private:
+   int dimension;
+   double* numbers;
+
+public:
+    MyVector(double numList[], int dimension);
     MyVector();
-    void print();
-    MyVector operator+(MyVector vec);
-    double operator*(MyVector vec);
-    MyVector operator/(MyVector vec);
-    MyVector operator-(MyVector vec);
+    void print() const;
+    MyVector operator+(const MyVector& vec) const;
+    MyVector operator-(const MyVector& vec) const;
+    double operator*(const MyVector& vec) const;
+    MyVector operator/(const MyVector& vec) const;
+
+    string to_string() const;
+    int get_dimension() const;
+    double get(int index) const;
 };
+
+void print_error(char op, const MyVector& a, const MyVector& b);
+void print_error(char op, double a, const MyVector& b);
 
 #endif
