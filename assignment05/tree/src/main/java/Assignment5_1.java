8531a641132f27b71b5b70e574af26cdfa85b92c
pbzweihander <sd852456@naver.com>
2017-12-07 18:38:47 +0900

    WIP

--- assignment05/tree/src/main/java/Assignment5_1.java
+++ assignment05/tree/src/main/java/Assignment5_1.java
@@ -1,11 +1,37 @@
 public class Assignment5_1 {
-    public Node5_1 root;
-    public Assignement5_1(String preorder, String inorder) {
+    private class StringList {
+        private class CharacterNode {
+            public char value;
+            public CharacterNode next;
+
+            public CharacterNode(char c) {
+                value = c;
+                next = null;
+            }
+        }
+
+        private CharacterNode root;
+        private CharacterNode tail;
 
     }
 
+    public Node5_1 root;
+
+    public Assignment5_1(String preorder, String inorder) {
+        if (preorder.isEmpty()) {
+            root = null;
+        } else {
+        }
+    }
+
+    private Node5_1 construct(String preorder, String inorder) {
+        Node5_1 node = new Node5_1();
+        node.label = preorder.charAt(0);
+        node.left = construct(preorder.substring(1), inorder.split("" + node.label)[0]);
+    }
+
     public String report() {
-        return ""
+        return "";
     }
 
     public Node5_1 get() {
