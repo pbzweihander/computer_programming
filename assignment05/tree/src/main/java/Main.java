61f4602ff5303044d4cbb1d21cef51209315f4bc
pbzweihander <sd852456@naver.com>
2017-12-08 14:05:39 +0900

    kipa's checker

--- assignment05/tree/src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Assignment5_1 tree = new Assignment5_1("ABDHIEJKCFLMGNO", "HDIBJEKALFMCNGO");
-        System.out.println(tree.report());
-    }
-}
