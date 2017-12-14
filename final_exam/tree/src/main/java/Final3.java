c682ff42bd23638b969e42df23a3b4437cda6975
pbzweihander <sd852456@naver.com>
2017-12-14 19:42:24 +0900

    final-3 tree

---
+++ final_exam/tree/src/main/java/Final3.java
@@ -0,0 +1,32 @@
+//Student   Name:
+//Student Number:
+//Do not import any java libraries
+//Do not write extra classes
+public class Final3 {
+    public Node root;
+
+    //Do not create any field varable
+    public Final3(String s) {
+        //handle case s = "" and passing root to report(FinalNode n)
+        //Modify as you wish
+    }
+
+    private void decode() {
+        //Modify return type and parameter as you wish to build tree recursively
+        //Modify as you wish
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public String encode(Node otherTreeRoot) { //you may change parameter name
+        String s = "output the label of the node";
+        s += " - encode the left subtree";
+        s += " - encode the right subtree";
+        s += "- output label of the node again.";
+        //Modify as you wish
+        return s;
+    }
+    //Do not write any other method than provided
+}
---
+++ final_exam/tree/src/main/java/Final3Test.java
@@ -0,0 +1,30 @@
+public class Final3Test {
+    public static void main(String[] args) {
+        Final3 tree = new Final3("ABDFFGGDEEBCCA");
+        if (inorder(tree.getRoot()).compareTo("FDGBEAC") == 0)
+            System.out.println("match1");
+        else
+            System.out.println("nope1");
+
+        Final3 tree2 = new Final3("");
+        if (tree2.report(tree.getRoot()).compareTo("ABDFFGGDEEBCCA") == 0)
+            System.out.println("match2");
+        else
+            System.out.println("nope2");
+
+        if (tree2.report(tree2.getRoot()).compareTo("") == 0)
+            System.out.println("match3");
+        else
+            System.out.println("nope3");
+    }
+
+    public static String inorder(Node n) {
+        String s = "";
+        if (n.left != null)
+            s += inorder(n.left);
+        s += n.label;
+        if (n.right != null)
+            s += inorder(n.right);
+        return s;
+    }
+}
